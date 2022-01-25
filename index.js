d5fd4b9638a0e335404bfbd6e9f2e512c27cfbf6
mahabubimon <mahabubemon24@gmail.com>
2022-01-26 02:49:20 +0600

    update

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB URI
-const uri =
-  "mongodb+srv://travelcoinnfts:travelcoinnfts12@cluster0.ojn33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ojn33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 // Create Client in MongoDB
 const client = new MongoClient(uri, {
@@ -29,39 +28,13 @@ const run = async () => {
   try {
     await client.connect();
     const database = client.db("travel-coin");
-    const africaCollection = database.collection("africa");
-    const americaCollection = database.collection("america");
-    const antarcticaCollection = database.collection("antarqtica");
-    const asiaCollection = database.collection("asia");
-    const europeCollection = database.collection("europe");
-    const oceaniaCollection = database.collection("oceania");
 
     // Get API
-    app.get("/africa", async (req, res) => {
-      const africa = await africaCollection.find({}).toArray();
-      res.send(africa);
-    });
-    app.get("/america", async (req, res) => {
-      const america = await americaCollection.find({}).toArray();
-      res.send(america);
-    });
-
-    app.get("/antarctica", async (req, res) => {
-      const antarctica = await antarcticaCollection.find({}).toArray();
-      res.send(antarctica);
-    });
-
-    app.get("/asia", async (req, res) => {
-      const asia = await asiaCollection.find({}).toArray();
-      res.send(asia);
-    });
-    app.get("/europe", async (req, res) => {
-      const europe = await europeCollection.find({}).toArray();
-      res.send(europe);
-    });
-    app.get("/oceania", async (req, res) => {
-      const oceania = await oceaniaCollection.find({}).toArray();
-      res.send(oceania);
+    app.get("/:region", async (req, res) => {
+      const region = req.params.region;
+      const regionCollection = database.collection(region);
+      const regionData = await regionCollection.find({}).toArray();
+      res.send(regionData);
     });
   } catch {
   } finally {
